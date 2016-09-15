aa6fbbc3e212587997fb97fa92d3e47f201b43b8
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-09-15 15:21:25 -0700

    Add Tag page and view.

---
+++ src/Flickr_Clone/Controllers/TagController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Flickr_Clone.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flickr_Clone.Controllers
+{
+    public class TagController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public TagController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Tag> tags = _db.Tags.ToList();
+            return View(tags);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Tag tag = _db.Tags.Include(t => t.ImageTags)
+                    .ThenInclude(it => it.Image)
+                .FirstOrDefault(t => t.TagId == id);
+            return View(tag);
+        }
+    }
+}
